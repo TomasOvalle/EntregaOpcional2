d701c6ce46fdae66956886ef06c9b5b27627f958
Tomás <tomasovallec[200~tomasovallec>
2024-06-03 12:42:06 -0400

    EntregaOpcional2

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # EntregaOpcional2
+
+En el caso que pruebe las rutas desde el front, debe hacerlo de la siguiente manera:
+    ej: http://localhost:8080/pages/index.html
---
+++ public/scripts/navBar.js
@@ -0,0 +1,37 @@
+async function printConditions() {
+    try {
+        let template = "";
+        let online = await fetch("/api/sessions/online", { credentials: 'include' });
+        online = await online.json();
+        if (online.statusCode === 200) {
+            template = `
+                <button class="btn" class="signOut-button" id="SignOut">Sign Out!</button>
+                    <a href="./carts.html">
+                        <input class="btn" type="button" value="Cart">
+                    </a>`;
+                document.querySelector("#conditions").innerHTML = template;
+                document.querySelector("#SignOut").addEventListener("click", async () => {
+                    const opts = {
+                        method: "POST",
+                        credentials: "include",
+                        headers: { "Content-Type": "application/json" },
+                    };
+                    let response = await fetch("/api/sessions/signout", opts);
+                    response = await response.json();
+                    console.log(response);
+                    if (response.statusCode === 200) {
+                        location.replace("/pages/index.html")
+                    }
+                })
+        } else {
+            template = `<a href="./login.html">
+                            <input class="btn" type="button" name="Login" value="Login">
+                        </a>`
+            document.querySelector("#conditions").innerHTML = template;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+printConditions();
